aed1107beb122f47b601168008408e87ab02b724
Mattèo Gauthier <matteo.gauthier@gmail.com>
2022-07-28 14:04:10 +0200

    [APP] Init template

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="font-medium text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+        Mako template from Squale.Agency
+      </h1>
+    </div>
+  )
+}
